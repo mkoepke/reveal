9fe3d32b2093ec81f86e46898eadf214429c2c8b
Mike Koepke <mike.koepke@gmail.com>
2016-05-23 12:04:42 -0500

    Initial fork check in

--- jquery.reveal.js
+++ jquery.reveal.js
@@ -1,14 +1,14 @@
 /*
- * jQuery Reveal Plugin 1.0
- * www.ZURB.com
- * Copyright 2010, ZURB
+ * jQuery Reveal Plugin 1.2 Fork
+ * Copyright 2010, ZURB ( www.ZURB.com )
  * Free to use under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
+ * Updated by Mike Koepke
 */
 
 
-(function ($) {
-  $('a[data-reveal-id]').live('click', function (event) {
+jQuery(document).ready(function($) {
+	$(document).on('click','a[data-reveal-id]',function (event) {
     event.preventDefault();
     var modalLocation = $(this).attr('data-reveal-id');
     $('#' + modalLocation).reveal($(this).data());
@@ -16,7 +16,7 @@
 
   $.fn.reveal = function (options) {
     var defaults = {
-      animation: 'fadeAndPop',                // fade, fadeAndPop, none
+      animation: 'none',                      // fade, fadeAndPop, none
       animationSpeed: 300,                    // how fast animtions are
       closeOnBackgroundClick: true,           // if you click background will modal close?
       dismissModalClass: 'close-reveal-modal' // the class of a button or element that will close an open modal
@@ -29,6 +29,9 @@
         topOffset  = modal.height() + topMeasure,
         locked     = false,
         modalBg    = $('.reveal-modal-bg');
+      var outerHeight = $(window).height();
+      var height = modal.outerHeight();
+      var top;
 
       if (modalBg.length == 0) {
         modalBg = $('<div class="reveal-modal-bg" />').insertAfter(modal);
@@ -41,7 +44,7 @@
         if (!locked) {
           lockModal();
           if (options.animation == "fadeAndPop") {
-            modal.css({'top': $(document).scrollTop() - topOffset, 'opacity': 0, 'visibility': 'visible'});
+            modal.css({'top': $(document).scrollTop() - topOffset, 'opacity': 0, 'visibility': 'visible', 'display': 'block'});
             modalBg.fadeIn(options.animationSpeed / 2);
             modal.delay(options.animationSpeed / 2).animate({
               "top": $(document).scrollTop() + topMeasure + 'px',
@@ -49,14 +52,19 @@
             }, options.animationSpeed, unlockModal);
           }
           if (options.animation == "fade") {
-            modal.css({'opacity': 0, 'visibility': 'visible', 'top': $(document).scrollTop() + topMeasure});
+            modal.css({'opacity': 0, 'visibility': 'visible', 'display': 'block', 'top': $(document).scrollTop() + topMeasure});
             modalBg.fadeIn(options.animationSpeed / 2);
             modal.delay(options.animationSpeed / 2).animate({
               "opacity": 1
             }, options.animationSpeed, unlockModal);
           }
           if (options.animation == "none") {
-            modal.css({'visibility': 'visible', 'top': $(document).scrollTop() + topMeasure});
+            if (height > outerHeight) {
+              top = parseInt(Math.min(100, outerHeight / 10), 10);
+            } else {
+              top = parseInt((outerHeight - height) / 4, 10)  + topMeasure;
+            }
+            modal.css({'visibility': 'visible', 'display': 'block', 'top': top + 'px' });
             modalBg.css({"display": "block"});
             unlockModal();
           }
@@ -74,7 +82,7 @@
               "top":  $(document).scrollTop() - topOffset + 'px',
               "opacity": 0
             }, options.animationSpeed / 2, function () {
-              modal.css({'top': topMeasure, 'opacity': 1, 'visibility': 'hidden'});
+              modal.css({'top': topMeasure, 'opacity': 1, 'visibility': 'hidden', 'display': 'none'});
               unlockModal();
             });
           }
@@ -83,14 +91,15 @@
             modal.animate({
               "opacity" : 0
             }, options.animationSpeed, function () {
-              modal.css({'opacity': 1, 'visibility': 'hidden', 'top': topMeasure});
+              modal.css({'opacity': 1, 'visibility': 'hidden', 'display': 'none', 'top': topMeasure});
               unlockModal();
             });
           }
           if (options.animation == "none") {
-            modal.css({'visibility': 'hidden', 'top': topMeasure});
+            modal.css({'visibility': 'hidden', 'display': 'none', 'top': topMeasure});
             modalBg.css({'display': 'none'});
           }
+          close_video();
         }
         modal.unbind('reveal:close', closeAnimation);
       }
@@ -121,6 +130,25 @@
       function lockModal() {
         locked = true;
       }
+
+      function close_video() {
+          var iframe = $('iframe', modal);
+
+          if (iframe.length > 0) {
+            //First get the  iframe URL
+            var url = iframe.attr('src');
+
+            //Then assign the src to null, this then stops the video been playing
+            iframe.attr('src', '');
+
+            // Finally you reasign the URL back to your iframe, so when you hide and load it again you still have the link
+              iframe.attr("src", url.replace("autoplay=1", "autoplay=0"));
+//              video.hide();
+          }
+      }
+
     });
+
   };
-})(jQuery);
+});
+
